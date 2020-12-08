2fb5e024c51426dc9233900c82b4ebb0aaf36cdf
Jon Petter Grandal <jon.petter.grandal@dnvgl.com>
2020-12-08 22:51:09 +0100

    Some refactoring - still a train wreck

--- AoC2020/Day8/Solver.cs
+++ AoC2020/Day8/Solver.cs
@@ -6,7 +6,7 @@ namespace AoC2020.Day8
 {
     internal static class Solver
     {
-        public static void Task1(string[] program)
+        public static void Task1(string[] program) // Answer: 1766
         {
             var accounting = new int[program.Length];
 
@@ -45,51 +45,45 @@ namespace AoC2020.Day8
             Console.WriteLine(accumulator);
         }
 
-        public static void Task2(string[] program)
+        public static void Task2(string[] program) // Answer: 1639
         {
-            var (failedLine, infiniteLoop, accounting, instrToAlter, accumulator) = RunProgram(program);
+            var (_, instrToAlter) = RunProgram(program);
 
             instrToAlter.Sort((a, b) => b.Item2.CompareTo(a.Item2));
 
-            var lastLine = accounting.Max();
-            var tempProg = new string[program.Length];
-            program.CopyTo(tempProg, 0);
+            var testProgram = new string[program.Length];
+            program.CopyTo(testProgram, 0);
 
             foreach (var (line, counter) in instrToAlter)
             {
                 var instruction = program[line].Substring(0, 3);
-                switch (instruction)
+                testProgram[line] = instruction switch
                 {
-                    case "jmp":
-                        tempProg[line] = tempProg[line].Replace("jmp", "nop");
-                        break;
-                    case "nop":
-                        tempProg[line] = tempProg[line].Replace("nop", "jmp");
-                        break;
-                }
+                    "jmp" => testProgram[line].Replace("jmp", "nop"),
+                    "nop" => testProgram[line].Replace("nop", "jmp"),
+                    _ => testProgram[line]
+                };
 
-                var quadruple = RunProgram(tempProg);
-                if (quadruple.Item2)
+                var (infiniteLoop, _) = RunProgram(testProgram);
+                if (infiniteLoop)
                 {
-                    program.CopyTo(tempProg, 0);
+                    program.CopyTo(testProgram, 0);
                     continue;
                 }
 
-                Console.WriteLine($"Accumulator: {quadruple.Item5}");
                 break;
             }
         }
 
-        private static (int, bool, int[], List<(int, int)>, int) RunProgram(string[] program)
+        private static (bool, List<(int, int)>) RunProgram(string[] program)
         {
             var accounting = new int[program.Length];
 
             var accumulator = 0;
-
             var counter = 0;
             var line = 0;
 
-            bool inifiniteLoop;
+            bool infiniteLoop;
 
             var instrToAlter = new List<(int, int)>();
 
@@ -97,13 +91,15 @@ namespace AoC2020.Day8
             {
                 if (line >= program.Length)
                 {
-                    inifiniteLoop = false;
+                    infiniteLoop = false;
+                    Console.WriteLine($"Accumulator at end of program: {accumulator}");
                     break;
                 }
 
                 if (accounting[line] > 0)
                 {
-                    inifiniteLoop = true;
+                    infiniteLoop = true;
+                    Console.WriteLine($"Accumulator after one run of infinite loop: {accumulator}");
                     break;
                 }
 
@@ -133,10 +129,7 @@ namespace AoC2020.Day8
                 }
             }
 
-            Console.WriteLine(accumulator);
-
-            (int, bool, int[], List<(int, int)>, int) result = (line, inifiniteLoop, accounting, instrToAlter,
-                accumulator);
+            (bool, List<(int, int)>) result = (infiniteLoop, instrToAlter);
 
             return result;
         }
